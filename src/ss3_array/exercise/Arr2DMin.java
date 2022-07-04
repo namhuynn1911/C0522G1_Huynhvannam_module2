1b73b03e66fe08eb28b759ab52e075af88e7fa99
namhuynn1911 <namhuynh191193@gmail.com>
2022-07-04 19:28:13 +0700

    neww

--- src/ss3_array/exercise/Arr2DMin.java
+++ src/ss3_array/exercise/Arr2DMin.java
@@ -2,7 +2,7 @@ package ss3_array.exercise;
 
 import java.util.Scanner;
 
-public class Arr2DMax {
+public class Arr2DMin {
     public static void main(String[] args) {
         int crow, col;
         Scanner scanner = new Scanner(System.in);
@@ -18,17 +18,16 @@ public class Arr2DMax {
                 arr[i][j] = scanner.nextInt();
             }
         }
-        int max=arr[0][0];
+        int min = arr[0][0];
         for(int i=0;i<arr.length;i++) {
-            for (int j=0;j<arr[i].length;j++) {
+            for(int j=0;j<arr[i].length;j++) {
                 System.out.print(arr[i][j]+"\t");
-                if(arr[i][j]>max) {
-                    max=arr[i][j];
+                if(arr[i][j]<min) {
+                    min=arr[i][j];
                 }
             }
             System.out.println();
         }
-        System.out.printf("số lớn nhất trong mảng là %d",max);
-
+        System.out.println("giá trị nhỏ nhất là :"+min);
     }
 }
